9d090aff8ec36dc48e63f40c4c7973dc270c814f
MaximGasilin <63611476+MaximGasilin@users.noreply.github.com>
2020-06-24 00:59:16 +0300

    Update lesson-4.sh

--- lesson-4/lesson-4.sh
+++ lesson-4/lesson-4.sh
@@ -1,110 +1,119 @@
-# Начало работы в терминале над домашним заданием к 3-му уроку
-ubuntu@ip-172-31-27-165:~$ mkdir lesson_3
-ubuntu@ip-172-31-27-165:~$ ls
-lesson_3
-ubuntu@ip-172-31-27-165:~$ cd lesson_3
-
-# 1. Создание с помощью текстового редактора Vim создать файл с программой на Python, 
-# выводящей текст Hello, world!
-ubuntu@ip-172-31-27-165:~/lesson_3$ vim les_3_task_1.py
-
-# Для проверки того, что в вим был создан файл нужно смотреть скриншот les_3_task_1_in_vim.JPG
-
-ubuntu@ip-172-31-27-165:~/lesson_3$ cat les_3_task_1.py
+# Начало работы в терминале над домашним заданием к 4-му уроку
+ubuntu@ip-172-31-44-150:~$ mkdir lesson_4
+ubuntu@ip-172-31-44-150:~$ cd lesson_4
+ubuntu@ip-172-31-44-150:~/lesson_4$ la
+ubuntu@ip-172-31-44-150:~/lesson_4$ vim les_4_task_1.py
+ubuntu@ip-172-31-44-150:~/lesson_4$ la
+les_4_task_1.py
+ubuntu@ip-172-31-44-150:~/lesson_4$ cat les_4_task_1.py
 print('Hello world!')
+ubuntu@ip-172-31-44-150:~/lesson_4$ ls -l les_4_task_1.py
+-rw-rw-r-- 1 ubuntu ubuntu 22 Jun 23 20:36 les_4_task_1.py
 
-# 2. Запустить команду, определяющую число строк в файле. Но предварительно свериться с мануалом. Т.к. опыта мало.
-ubuntu@ip-172-31-27-165:~/lesson_3$ man wc
-ubuntu@ip-172-31-27-165:~/lesson_3$ wc -l les_3_task_1.py
-1 les_3_task_1.py
 
-# 3. Создать еще один файл с командой на Python, выводящей текст Linear regression.
-# Опять задействуем vim
-ubuntu@ip-172-31-27-165:~/lesson_3$ vim les_3_task_3.py
+# 1. Создать пользователя user_new и предоставить ему права на редактирование файла с программой, 
+# выводящей на экран Hello, world!
+ubuntu@ip-172-31-44-150:~/lesson_4$ sudo useradd -m -s /bin/bash user_new
+ubuntu@ip-172-31-44-150:~/lesson_4$ sudo passwd user_new
+New password:
+Retype new password:
+passwd: password updated successfully
 
-# Для проверки того, что в вим был создан файл нужно смотреть скриншот les_3_task_3_in_vim.JPG
+ubuntu@ip-172-31-44-150:~/lesson_4$ sudo usermod -a -G ubuntu user_new
+ubuntu@ip-172-31-44-150:~/lesson_4$ groups user_new
+user_new : user_new ubuntu
 
-ubuntu@ip-172-31-27-165:~/lesson_3$ cat les_3_task_3.py
-print('Linear regression.')
+ubuntu@ip-172-31-44-150:~/lesson_4$ su user_new
+Password:
+user_new@ip-172-31-44-150:/home/ubuntu/lesson_4$ ls -l les_4_task_1.py
+-rw-rw-r-- 1 ubuntu ubuntu 22 Jun 23 20:36 les_4_task_1.py
 
-# 4. Объединить эти два файла с помощью команды cat.
-ubuntu@ip-172-31-27-165:~/lesson_3$ cat les_3_task_1.py les_3_task_3.py > les_3_task_4.py
-ubuntu@ip-172-31-27-165:~/lesson_3$ cat les_3_task_4.py
+user_new@ip-172-31-44-150:/home/ubuntu/lesson_4$ echo "print('Hello Luna')" >> les_4_task_1.py
+user_new@ip-172-31-44-150:/home/ubuntu/lesson_4$ cat les_4_task_1.py
 print('Hello world!')
-print('Linear regression.')
+print('Hello Luna')
 
-# 5. Придумать три случая применения команды cat для работы с текстовыми файлами.
-
-# Создание небольшой программы на python
-ubuntu@ip-172-31-27-165:~/lesson_3$ vim les_3_task_5.py
-# Проверка, что программа записалась. Пустые строки в ней нужны для проверки работы функции cat
-ubuntu@ip-172-31-27-165:~/lesson_3$ cat les_3_task_5.py
-list_of_products = ['Молоко', 'Хлеб', 'Курица', 'Картофель', 'Сало']
-
-print('Список покупок:')
-
-list(map(print, list_of_products))
-
-print('Хорошего дня!')
-
-# Пронумеруем все строки
-ubuntu@ip-172-31-27-165:~/lesson_3$ cat -n les_3_task_5.py
-     1  list_of_products = ['Молоко', 'Хлеб', 'Курица', 'Картофель', 'Сало']
-     2
-     3  print('Список покупок:')
-     4
-     5  list(map(print, list_of_products))
-     6
-     7  print('Хорошего дня!')
-	 
-# Пронумеруем не пустые строки	 
-ubuntu@ip-172-31-27-165:~/lesson_3$ cat -b les_3_task_5.py
-     1  list_of_products = ['Молоко', 'Хлеб', 'Курица', 'Картофель', 'Сало']
-
-     2  print('Список покупок:')
-
-     3  list(map(print, list_of_products))
-
-     4  print('Хорошего дня!')
-
-# Пронумеруем все строки и выведем символа $ в конце каждой строки	 
-ubuntu@ip-172-31-27-165:~/lesson_3$ cat -n -E les_3_task_5.py
-     1  list_of_products = ['Молоко', 'Хлеб', 'Курица', 'Картофель', 'Сало']$
-     2  $
-     3  print('Список покупок:')$
-     4  $
-     5  list(map(print, list_of_products))$
-     6  $
-     7  print('Хорошего дня!')$
-	 
-# Вывод всех непечатных символов	 
-ubuntu@ip-172-31-27-165:~/lesson_3$ cat -A les_3_task_5.py
-list_of_products = ['M-PM-^\M-PM->M-PM-;M-PM->M-PM-:M-PM->', 'M-PM-%M-PM-;M-PM-5M-PM-1', 'M-PM-^ZM-QM-^CM-QM-^@M-PM-8M-QM-^FM-PM-0', 'M-PM-^ZM-PM-0M-QM-^@M-QM-^BM-PM->M-QM-^DM-PM-5M-PM-;M-QM-^L', 'M-PM-!M-PM-0M-PM-;M-PM->']$
-$
-print('M-PM-!M-PM-?M-PM-8M-QM-^AM-PM->M-PM-: M-PM-?M-PM->M-PM-:M-QM-^CM-PM-?M-PM->M-PM-::')$
-$
-list(map(print, list_of_products))$
-$
-print('M-PM-%M-PM->M-QM-^@M-PM->M-QM-^HM-PM-5M-PM-3M-PM-> M-PM-4M-PM-=M-QM-^O!')$
-
-# Добавление к файлу les_3_task_5.py данных из les_3_task_1.py и les_3_task_3.py
-ubuntu@ip-172-31-27-165:~/lesson_3$ cat les_3_task_1.py les_3_task_3.py >> les_3_task_5.py
-
-# Проверка последнего действия
-ubuntu@ip-172-31-27-165:~/lesson_3$ cat les_3_task_5.py
-list_of_products = ['Молоко', 'Хлеб', 'Курица', 'Картофель', 'Сало']
-
-print('Список покупок:')
-
-list(map(print, list_of_products))
-
-print('Хорошего дня!')
+# 2. Зайти под юзером user_new и с помощью редактора Vim поменять фразу 
+# в скрипте из пункта 1 на любую другую.
+user_new@ip-172-31-44-150:/home/ubuntu/lesson_4$ cat les_4_task_1.py
 print('Hello world!')
-print('Linear regression.')
+print('Hello Luna')
+user_new@ip-172-31-44-150:/home/ubuntu/lesson_4$ vim les_4_task_1.py
+user_new@ip-172-31-44-150:/home/ubuntu/lesson_4$ cat les_4_task_1.py
+print('Hello Mars!')
+print('Hello Sun!!!')
 
-# Удаление всех файлов и каталога lesson_3 для очистки места и приведения рабочего каталога в порядок.
-ubuntu@ip-172-31-27-165:~/lesson_3$ cd ..
-ubuntu@ip-172-31-27-165:~$ rm -r lesson_3/
-ubuntu@ip-172-31-27-165:~$ ls
-ubuntu@ip-172-31-27-165:~$
+
+# 3.* Под юзером user_new зайти в его домашнюю директорию и создать программу на Python, 
+# выводящую в консоль цифры от 1 до 10 включительно с интервалом в 1 секунду.
+
+user_new@ip-172-31-44-150:/home/ubuntu/lesson_4$ cd ~
+user_new@ip-172-31-44-150:~$ whoami
+user_new
+user_new@ip-172-31-44-150:~$ vim les_4_task_2.py
+user_new@ip-172-31-44-150:~$ cat les_4_task_2.py
+from time import sleep
+
+for i in range(1, 11):
+    print(i)
+    sleep(1)
+user_new@ip-172-31-44-150:~$ python3 --version
+Python 3.8.2
+user_new@ip-172-31-44-150:~$ python3 les_4_task_2.py
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+# интервал между выводом действительно составлял 1 секунду
+
+# Еще один способ. С помощью строки #!/usr/bin/python3
+user_new@ip-172-31-44-150:~$ vim les_4_task_2.py
+user_new@ip-172-31-44-150:~$ cat les_4_task_2.py
+#!/usr/bin/python3
+from time import sleep
+
+for i in range(1, 11):
+    print(i)
+    sleep(1)
+
+user_new@ip-172-31-44-150:~$ chmod u+x les_4_task_2.py
+user_new@ip-172-31-44-150:~$ ls -l les_4_task_2.py
+-rwxrw-r-- 1 user_new user_new 92 Jun 23 21:21 les_4_task_2.py
+
+user_new@ip-172-31-44-150:~$ les_4_task_2.py
+les_4_task_2.py: command not found
+
+user_new@ip-172-31-44-150:~$ ./les_4_task_2.py
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+# интервал между выводом действительно составлял 1 секунду
+
+# Удаление всех файлов и каталога lesson_4 для очистки места и приведения рабочего каталога в порядок.
+user_new@ip-172-31-44-150:~$ la
+.bash_logout  .les_4_task_2.py.swp  .profile  les_4_task_2.py
+.bashrc       .local                .viminfo
+user_new@ip-172-31-44-150:~$ rm les_4_task_2.py
+
+user_new@ip-172-31-44-150:~$ su ubuntu
+Password:
+ubuntu@ip-172-31-44-150:~$ cd ~
+
+ubuntu@ip-172-31-27-150:~/lesson_4$ cd ..
+ubuntu@ip-172-31-27-150:~$ rm -r lesson_4/
+ubuntu@ip-172-31-27-150:~$ ls
+ubuntu@ip-172-31-27-150:~$
 
